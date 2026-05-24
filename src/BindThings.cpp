10244996181eee573d07e2467b9ce68131fa16b2
getbindthings <getbindthings@gmail.com>
2026-05-24 22:22:22 +0400

    Update BindThings.cpp

--- src/BindThings.cpp
+++ src/BindThings.cpp
@@ -6,11 +6,10 @@ BindThings::BindThings(const char* token)
   : _token(token), _mqttClient(_wifiClient), _commandCb(nullptr)
 {
   _instance = this;
-  snprintf(_topicTelemetry,  sizeof(_topicTelemetry),  "devices/%s/telemetry",  token);
-  snprintf(_topicStatus,     sizeof(_topicStatus),     "devices/%s/status",     token);
-  snprintf(_topicCommands,   sizeof(_topicCommands),   "devices/%s/commands",   token);
-  snprintf(_topicAttributes, sizeof(_topicAttributes), "devices/%s/attributes", token);
-  snprintf(_clientId,        sizeof(_clientId),        "bt_%08lX", ESP.getChipId ? (unsigned long)ESP.getChipId() : (unsigned long)esp_random());
+  snprintf(_topicTelemetry, sizeof(_topicTelemetry), "devices/%s/telemetry", token);
+  snprintf(_topicStatus,    sizeof(_topicStatus),    "devices/%s/status",    token);
+  snprintf(_topicCommands,  sizeof(_topicCommands),  "devices/%s/commands",  token);
+  snprintf(_clientId,       sizeof(_clientId),       "bt_%08X", ESP.getChipId());
 }
 
 bool BindThings::connect(const char* ssid, const char* password) {
@@ -27,19 +26,23 @@ bool BindThings::connect(const char* ssid, const char* password) {
       return false;
     }
   }
-  Serial.print("\n[BindThings] WiFi connected: ");
+  Serial.print("\n[BindThings] WiFi IP: ");
   Serial.println(WiFi.localIP());
 
   return _connectMQTT();
 }
 
 bool BindThings::_connectMQTT() {
-  _wifiClient.setInsecure(); // TLS without certificate validation
+#ifdef ESP8266
+  _wifiClient.setInsecure();
+#else
+  _wifiClient.setInsecure();
+#endif
+
   _mqttClient.setServer(BT_BROKER, BT_PORT);
   _mqttClient.setCallback(_mqttCallback);
   _mqttClient.setBufferSize(512);
 
-  // LWT payload
   String lwtPayload = "{\"online\":false,\"reason\":\"lwt\"}";
 
   Serial.print("[BindThings] Connecting to MQTT...");
@@ -66,7 +69,7 @@ bool BindThings::_connectMQTT() {
 
 bool BindThings::loop() {
   if (!_mqttClient.connected()) {
-    Serial.println("[BindThings] MQTT disconnected, reconnecting...");
+    Serial.println("[BindThings] Reconnecting...");
     _connectMQTT();
   }
   return _mqttClient.loop();
@@ -115,9 +118,7 @@ bool BindThings::send(const String& jsonPayload) {
 }
 
 bool BindThings::setOnline(bool online) {
-  String payload = online
-    ? "{\"online\":true}"
-    : "{\"online\":false}";
+  String payload = online ? "{\"online\":true}" : "{\"online\":false}";
   return _mqttClient.publish(_topicStatus, payload.c_str(), true);
 }
 
@@ -130,15 +131,12 @@ bool BindThings::isConnected() {
 }
 
 void BindThings::_mqttCallback(char* topic, byte* payload, unsigned int length) {
-  if (_instance) {
-    _instance->_handleMessage(topic, payload, length);
-  }
+  if (_instance) _instance->_handleMessage(topic, payload, length);
 }
 
 void BindThings::_handleMessage(char* topic, byte* payload, unsigned int length) {
   String msg = "";
   for (unsigned int i = 0; i < length; i++) msg += (char)payload[i];
-
   if (String(topic) == String(_topicCommands) && _commandCb) {
     _commandCb(msg);
   }
