b5d946f05e7f6f8f0022497a74f391aeda61f580
getbindthings <getbindthings@gmail.com>
2026-05-24 22:22:43 +0400

    Update BindThings.h

--- src/BindThings.h
+++ src/BindThings.h
@@ -2,13 +2,21 @@
 #define BINDTHINGS_H
 
 #include <Arduino.h>
-#include <WiFiClientSecure.h>
+
+#ifdef ESP8266
+  #include <ESP8266WiFi.h>
+  #include <WiFiClientSecureBearSSL.h>
+#else
+  #include <WiFi.h>
+  #include <WiFiClientSecure.h>
+#endif
+
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 
-#define BT_BROKER     "mqtt.bindthings.io"
-#define BT_PORT       8883
-#define BT_VERSION    "1.0.0"
+#define BT_BROKER  "mqtt.bindthings.io"
+#define BT_PORT    8883
+#define BT_VERSION "1.0.0"
 
 typedef void (*CommandCallback)(const String& payload);
 
@@ -16,27 +24,17 @@ class BindThings {
 public:
   BindThings(const char* token);
 
-  // WiFi + MQTT qoşul
   bool connect(const char* ssid, const char* password);
-
-  // Yenidən qoşulmağa cəhd et (loop-da çağır)
   bool loop();
 
-  // Tək dəyər göndər: bt.send("temperature", 25.5)
   bool send(const char* key, float value);
   bool send(const char* key, int value);
   bool send(const char* key, bool value);
   bool send(const char* key, const String& value);
-
-  // JSON string göndər: bt.send("{\"temp\":25.5}")
   bool send(const String& jsonPayload);
 
-  // Komanda callback: bt.onCommand(myCallback)
   void onCommand(CommandCallback cb);
-
-  // Online/offline status göndər
   bool setOnline(bool online = true);
-
   bool isConnected();
 
 private:
@@ -44,12 +42,16 @@ private:
   char _topicTelemetry[80];
   char _topicStatus[80];
   char _topicCommands[80];
-  char _topicAttributes[80];
   char _clientId[32];
 
+#ifdef ESP8266
+  BearSSL::WiFiClientSecure _wifiClient;
+#else
   WiFiClientSecure _wifiClient;
-  PubSubClient     _mqttClient;
-  CommandCallback  _commandCb;
+#endif
+
+  PubSubClient    _mqttClient;
+  CommandCallback _commandCb;
 
   bool _connectMQTT();
   static void _mqttCallback(char* topic, byte* payload, unsigned int length);
